9db12c1232439f90359d906c91c08c8061721dde
rizkyadiryanto14 <adiryantorizky140820@gmail.com>
2022-06-27 15:59:26 +0800

    update eror

---
+++ database/migrations/2022_06_27_145628_create_absensi_siswa_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absensi_siswa', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tanggal');
+            $table->integer('siswa_id');
+            $table->integer('kehadiran_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absensi_siswa');
+    }
+};
