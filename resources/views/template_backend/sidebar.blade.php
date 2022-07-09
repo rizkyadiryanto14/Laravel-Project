a523db1c04af1bd9bca60e3be0549160d9fa992b
rizkyadiryanto14 <adiryantorizky140820@gmail.com>
2022-07-09 09:49:06 +0800

    update kepala sekolah page

--- resources/views/template_backend/sidebar.blade.php
+++ resources/views/template_backend/sidebar.blade.php
@@ -11,7 +11,72 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                @if (Auth::user()->role == 'Admin' || Auth::user()->role == 'Operator')
+                @if (Auth::user()->role == 'Operator')
+                <li class="nav-item has-treeview" id="liDashboard">
+                    <a href="#" class="nav-link" id="Dashboard">
+                        <i class="nav-icon fas fa-home"></i>
+                        <p>
+                            Dashboard
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview ml-4">
+                        <li class="nav-item">
+                            <a href="{{ url('/') }}" class="nav-link" id="Home">
+                                <i class="fas fa-home nav-icon"></i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item has-treeview" id="liMasterData">
+                    <a href="#" class="nav-link" id="MasterData">
+                        <i class="nav-icon fas fa-edit"></i>
+                        <p>
+                            Laporan
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview ml-4">
+                        <li class="nav-item">
+                            <a href="{{ route('jadwal.index') }}" class="nav-link" id="DataJadwal">
+                                <i class="fas fa-calendar-alt nav-icon"></i>
+                                <p>Data Jadwal</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('guru.index') }}" class="nav-link" id="DataGuru">
+                                <i class="fas fa-users nav-icon"></i>
+                                <p>Data Guru</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('kelas.index') }}" class="nav-link" id="DataKelas">
+                                <i class="fas fa-home nav-icon"></i>
+                                <p>Data Kelas</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('siswa.index') }}" class="nav-link" id="DataSiswa">
+                                <i class="fas fa-users nav-icon"></i>
+                                <p>Data Siswa</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('mapel.index') }}" class="nav-link" id="DataMapel">
+                                <i class="fas fa-book nav-icon"></i>
+                                <p>Data Mapel</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('user.index') }}" class="nav-link" id="DataUser">
+                                <i class="fas fa-user-plus nav-icon"></i>
+                                <p>Data User</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                @elseif (Auth::user()->role == "Admin")
                 <li class="nav-item has-treeview" id="liDashboard">
                     <a href="#" class="nav-link" id="Dashboard">
                         <i class="nav-icon fas fa-home"></i>
@@ -82,7 +147,6 @@
                         </li>
                     </ul>
                 </li>
-                @if (Auth::user()->role == "Admin")
                 <li class="nav-item has-treeview" id="liViewTrash">
                     <a href="#" class="nav-link" id="ViewTrash">
                         <i class="nav-icon fas fa-recycle"></i>
@@ -130,10 +194,8 @@
                         </li>
                     </ul>
                 </li>
-                @else 
-                @endif
                 <li class="nav-item">
-                    <a href="http://localhost:9000/" class="nav-link" id="urk">
+                    <a href="https://ppdbsdit.herokuapp.com/" class="nav-link" id="urk">
                         <i class="fa fa-graduation-cap"></i>
                         <p>Penerimaan Siswa Baru</p>
                     </a>
